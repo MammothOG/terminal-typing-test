7610b5583391e5de727e60a64b3a2be786d5b6a1
MammothOG <felix.capuano@gmail.com>
2020-09-19 21:52:54 +0200

    working

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ NUMBER_WORDS_PRINTED = 3
 ROUND_NUMBER = 3
 cpm = []
 
-words = ["je", "suis", "en", "direction", "de", "paris"]
-
 def compute_cpm(time_ns, str_length):
     time_s = time_ns / 10**9
     cpm = (str_length * 60) / time_s
     return cpm
 
-def grab_words(words):
+def read_file(file_name):
+    words_list = []
+    with open(file_name, "r") as file:
+        raw = file.read()
+        words_list = raw.split("\n")
+
+    return words_list
+
+def pick_words(words):
     if len(words) == 0:
        return False
 
@@ -24,7 +30,6 @@ def grab_words(words):
     for _ in range(NUMBER_WORDS_PRINTED):
         word = random.choice(words)
         words_printed.append(word)
-        words.remove(word)
 
     return words_printed
 
@@ -44,43 +49,43 @@ def printParity(entries, words):
 
     print(parity + "\033[00m")
 
+#----------------------------------------------------------------
+words = read_file("./words-set/default-en.txt")
+
 for rounds in range(ROUND_NUMBER):
-    words_grabed = grab_words(words)
     entries = ""
     char = b""
 
-    if not words_grabed == False:
-        words_string = " ".join(words_grabed)
+    words_picked = pick_words(words)
+    words_string = " ".join(words_picked)
 
-        while not char == b"\r" or len(entries) < len(words_string):
+    while not char == b"\r" or len(entries) < len(words_string):
+        os.system("cls")
 
-            print("-- Round ", rounds, "--")
-            printParity(entries, words_string)
+        print("-- Round ", rounds, "--")
+        printParity(entries, words_string)
 
-            char = readchar.readchar()
-            if b'\x08' == char:
-                entries = entries[:-1]
-            elif b'\x1b' == char:
-                entries = ""
-            elif char in [b'\xfd']:
-                pass
-            else:
-                entries = entries + char.decode("utf-8")
+        char = readchar.readchar()
+        if b'\x08' == char:
+            entries = entries[:-1]
+        elif b'\x1b' == char:
+            entries = ""
+        elif char in [b'\xfd']:
+            pass
+        else:
+            entries = entries + char.decode("utf-8")
 
-            if len(entries) == 1:
-                start_time = time.time_ns()
+        if len(entries) == 1:
+            start_time = time.time_ns()
 
 
-            os.system("cls")
+    end_time = time.time_ns()
+    cpm.append(compute_cpm(end_time-start_time, len(words_string)))
 
-        end_time = time.time_ns()
-        cpm.append(compute_cpm(end_time-start_time, len(words_string)))
+cpm_mean = 0
+for i, cpm_round in enumerate(cpm):
+    print("Rounds {} : {}".format(i, cpm_round))
+    cpm_mean += cpm_round
 
-    else:
-        cpm_mean = 0
-        for i, cpm_round in enumerate(cpm):
-            print("Rounds {} : {}".format(i, cpm_round))
-            cpm_mean += cpm_round
-
-        cpm_mean /= len(cpm)
-        print("cpm mean : ", cpm_mean)
+cpm_mean /= len(cpm)
+print("cpm mean : ", cpm_mean)
